bd7b4dfb0de744db5e9d871f586194fed0543fe7
Mettteora <mrhens2013@gmail.com>
2023-04-08 20:18:07 +0200

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector([data - start]);
+const btnStop = document.querySelector([data - stop]);
 
+btnStart.addEventListener(click, startColorSwitch);
+btnStop.addEventListener(click, stopColorSwitch);
+
+let timeoutId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+function startColorSwitch() {
+  timeoutId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitch() {
+  clearInterval(timeoutId);
+  timeoutId = null;
+  console.log('stopColorSwitch called');
+}
+
+///////////
---
+++ src/js/02-timer.js
@@ -0,0 +1,18 @@
+const spanDays = document.querySelector([data - days]);
+const spanHours = document.querySelector([data - hours]);
+const spanMinutes = document.querySelector([data - minutes]);
+const spanSeconds = document.querySelector([data - seconds]);
+// const fliper = document.querySelector(#datetime-picker);
+console.log(spanDays);
+const options = {
+  enableTime: true,
+
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+// flatpickr(fliper, {});
