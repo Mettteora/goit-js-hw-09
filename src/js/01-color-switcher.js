e1668afed4e8cc0f5717cf42c006b0056f93fe16
Mettteora <mrhens2013@gmail.com>
2023-04-23 01:07:40 +0200

    321

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,28 @@
-const startButton = document.querySelector('button[data-start]');
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+const bodyRef = document.body;
 
-const btnStop = document.querySelector('button[data-stop]');
-console.log(startButton);
-startButton.addEventListener('click', startColorSwitch);
-btnStop.addEventListener('click', stopColorSwitch);
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStopRef.addEventListener('click', onBtnStopClick);
 
-let timeoutId = null;
+let INTERVAL_ID = null;
+function onBtnStartClick() {
+  INTERVAL_ID = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  btnStartRef.disabled = true;
+  btnStopRef.disabled = false;
+}
+
+function onBtnStopClick() {
+  clearInterval(INTERVAL_ID);
+  btnStartRef.disabled = false;
+  btnStopRef.disabled = true;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, '0')}`;
-}
-
-function startColorSwitch() {
-  timeoutId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-}
-
-function stopColorSwitch() {
-  clearInterval(timeoutId);
-  timeoutId = null;
-  console.log('stopColorSwitch called');
+    .padStart(6, 0)}`;
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,54 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', onFormSubmit);
+
+const form = {};
+
+function onFormInput(event) {
+  form[event.target.name] = Number(event.target.value);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = form;
+
+  if (delay <= 0 || step <= 0 || amount <= 0) {
+    Notiflix.Report.warning(
+      'Oops...',
+      'Typed number must be greater than 0',
+      'Got it!'
+    );
+    return;
+  }
+
+  for (let position = 0; position < amount; position += 1) {
+    createPromise(position + 1, delay + step * position)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
 
-// Change value of isSuccess variable to call resolve or reject
-const isSuccess = false;
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve('Success! Value passed to resolve function');
-    } else {
-      reject('Error! Error passed to reject function');
-    }
-  }, 2000);
-});
-
-// Will run first
-console.log('Before promise.then()');
-
-// Registering promise callbacks
-promise.then(
-  // onResolve will run third or not at all
-  value => {
-    console.log('onResolve call inside promise.then()');
-    console.log(value); // "Success! Value passed to resolve function"
-  },
-  // onReject will run third or not at all
-  error => {
-    console.log('onReject call inside promise.then()');
-    console.log(error); // "Error! Error passed to reject function"
-  }
-);
-
-// Will run second
-console.log('After promise.then()');
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
