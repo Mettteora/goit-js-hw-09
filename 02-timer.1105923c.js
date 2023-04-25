e976f2a91930898a7a7d86c96d980b5dc7da8611
Mettteora <95878092+Mettteora@users.noreply.github.com>
2023-04-25 15:45:38 +0000

    Deploying to gh-pages from @ Mettteora/goit-js-hw-09@5ae25c4d9a12d1332a028543c200d11400c933ad 🚀

--- 02-timer.1105923c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[data-days]"),document.querySelector("[data-hours]"),document.querySelector("[data-minutes]"),document.querySelector("[data-seconds]");
-//# sourceMappingURL=02-timer.1105923c.js.map
--- 03-promises.d590c0e7.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=new Promise((function(o,e){setTimeout((function(){e("Error! Error passed to reject function")}),2e3)}));console.log("Before promise.then()"),o.then((function(o){console.log("onResolve call inside promise.then()"),console.log(o)}),(function(o){console.log("onReject call inside promise.then()"),console.log(o)})),console.log("After promise.then()")}();
-//# sourceMappingURL=03-promises.d590c0e7.js.map
